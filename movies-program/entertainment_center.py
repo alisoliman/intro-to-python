7d00ce8666714b0757732586afa0442781ab81a1
Ali Soliman <ali.soliman95@gmail.com>
2016-10-14 23:40:05 +0200

    Learn classes and Objects

--- movies-program/entertainment_center.py
+++ movies-program/entertainment_center.py
@@ -2,4 +2,7 @@ import media
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://www.google.com",
                         "http://www.google.com")
-print (toy_story.storyline)
+toy_story.show_trailer()
+
+movies = [toy_story]
+
